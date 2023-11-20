89d1dc9ba6f8199cb2540edc981a5f0849381fe1
Ravil <75186385+Ravil377@users.noreply.github.com>
2023-11-19 19:34:24 -0900

    add menu

--- app/js/main.js
+++ app/js/main.js
@@ -187,14 +187,37 @@ const swiper = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"]('.banner-swipe
 
 const menu = document?.querySelector('[data-menu]');
 const menuToggle = document?.querySelector('[data-burger]');
+const catalogBtn = document?.querySelector('[data-catalogbtn]');
+const catalogMenu = document?.querySelector('[data-catalogmenu]');
+let isCatalogMenuOpen = false;
 menuToggle.addEventListener('change', function () {
   if (!this.checked) {
     menu?.classList.remove('menu--active');
   }
 });
-menu?.addEventListener('click', () => {
+menu?.addEventListener('click', e => {
+  e.preventDefault();
   menu?.classList.toggle('menu--active');
 });
+catalogBtn?.addEventListener('click', event => {
+  event.stopPropagation();
+  isCatalogMenuOpen = !isCatalogMenuOpen;
+  if (isCatalogMenuOpen) {
+    document.addEventListener('click', closeCatalogMenuHandler);
+  } else {
+    document.removeEventListener('click', closeCatalogMenuHandler);
+  }
+  catalogMenu?.classList.toggle('_active', isCatalogMenuOpen);
+});
+function closeCatalogMenuHandler(event) {
+  const isClickInsideCatalogMenu = catalogMenu?.contains(event.target);
+  const isClickOnCatalogBtn = catalogBtn?.contains(event.target);
+  if (!isClickInsideCatalogMenu && !isClickOnCatalogBtn) {
+    isCatalogMenuOpen = false;
+    catalogMenu?.classList.remove('_active');
+    document.removeEventListener('click', closeCatalogMenuHandler);
+  }
+}
 
 /***/ }),
 
