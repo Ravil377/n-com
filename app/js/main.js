25154a9927b47731b68cd83b0ece44fd6a46a7a3
Ravil <75186385+Ravil377@users.noreply.github.com>
2023-11-24 12:25:42 -0900

    fix catalog1

--- app/js/main.js
+++ app/js/main.js
@@ -191,10 +191,12 @@ const swiper = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"]('.banner-swipe
 /***/ (() => {
 
 const columns = document?.querySelector('[data-columns]');
+const goods = document?.querySelector('[data-goods]');
 columns?.addEventListener('click', e => {
   e.preventDefault();
   const currentBtn = e.target;
-  // const isActive = columns.classList.contains('menu--active');
+  const id = currentBtn?.dataset.id;
+  id == 1 && !goods.classList.contains('_one') ? goods?.classList.add('_one') : goods.classList.remove('_one');
   const active = columns?.querySelector('._active');
   active.classList.remove('_active');
   currentBtn.classList.add('_active');
