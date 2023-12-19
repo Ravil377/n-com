d8f80a25258e5221a591ad9534bb5aba26cce3ab
Ravil <75186385+Ravil377@users.noreply.github.com>
2023-12-18 23:50:40 -0900

    Updates

--- js/main.js
+++ js/main.js
@@ -518,14 +518,13 @@ document.addEventListener("DOMContentLoaded", function () {
     var screenWidth = window.innerWidth;
     if (screenWidth >= 1240) {
       if (window.scrollY >= 300) {
-        // console.log('dsfsddsf')
         headerBottom.classList.add("_fixed");
         header.classList.add('_pb');
       } else {
         headerBottom.classList.remove("_fixed");
         header.classList.remove('_pb');
       }
-      if (window.scrollY >= 500) {
+      if (window.scrollY >= 300) {
         headerBottom.classList.add("_transform");
       } else {
         headerBottom.classList.remove("_transform");
