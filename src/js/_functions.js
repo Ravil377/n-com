4f74be3754265e5a27814469b4bad0719f19b304
Ravil <75186385+Ravil377@users.noreply.github.com>
2024-04-21 20:49:33 -0800

    fix js

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,35 +1,5 @@
-// Данный файл - лишь собрание подключений готовых компонентов.
-// Рекомендуется создавать отдельный файл в папке components и подключать все там
-
-// Определение операционной системы на мобильных
-import { mobileCheck } from "./functions/mobile-check";
-
-
-// Определение ширины экрана
-// import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
-// if (isDesktop()) {
-//   console.log('...')
-// }
-
-// Троттлинг функции (для ресайза, ввода в инпут, скролла и т.д.)
-// import { throttle } from './functions/throttle';
-// let yourFunc = () => { console.log('throttle') };
-// let func = throttle(yourFunc);
-// window.addEventListener('resize', func);
-
 // Фикс фулскрин-блоков
 import './functions/fix-fullheight';
-
-
-
-// Реализация бургер-меню
-// import { burger } from './functions/burger';
-
-// Реализация остановки скролла (не забудьте вызвать функцию)
-import { disableScroll } from './functions/disable-scroll';
-
-// Реализация включения скролла (не забудьте вызвать функцию)
-import { enableScroll } from './functions/enable-scroll';
 
 // Реализация модального окна
 import GraphModal from 'graph-modal';
@@ -37,35 +7,11 @@ const modal = new GraphModal();
 
 // Реализация табов
 import GraphTabs from 'graph-tabs';
-const tabs = new GraphTabs('tab');
-
-// Получение высоты шапки сайта (не забудьте вызвать функцию)
-// import { getHeaderHeight } from './functions/header-height';
-
-// Подключение плагина кастом-скролла
-// import 'simplebar';
-
-// Подключение плагина для позиционирования тултипов
-// import { createPopper, right} from '@popperjs/core';
-// createPopper(el, tooltip, {
-//   placement: 'right'
-// });
-
-// Подключение свайпера
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// Swiper.use([Navigation, Pagination]);
-
-// const swiper = new Swiper(el, {
-//   slidesPerView: 'auto',
-// });
-
-// Подключение анимаций по скроллу
-// import AOS from 'aos';
-// AOS.init();
-
-// Подключение параллакса блоков при скролле
-// import Rellax from 'rellax';
-// const rellax = new Rellax('.rellax');
+const isTabs = document.querySelector('.tabs');
+var tabs;
+if(isTabs) {
+  tabs = new GraphTabs('tab');
+}
 
 // Подключение плавной прокрутки к якорям
 import SmoothScroll from 'smooth-scroll';
@@ -73,22 +19,8 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     offset: 150,
   });
 
-// Подключение событий свайпа на мобильных
-// import 'swiped-events';
-// document.addEventListener('swiped', function(e) {
-//   console.log(e.target);
-//   console.log(e.detail);
-//   console.log(e.detail.dir);
-// });
-
-// import { validateForms } from './functions/validate-forms';
-// const rules1 = [...];
-
-// const afterForm = () => {
-//   console.log('Произошла отправка, тут можно писать любые действия');
-// };
-
-ymaps.ready(init);
+const isMap = document.querySelector('.map1');
+isMap && ymaps.ready(init);
 
 function init() {
   var map = new ymaps.Map("map1", {
@@ -162,5 +94,3 @@ function init() {
   findAddresses(multipleAddressesData);
 }
 
-
-// validateForms('.form-1', rules1, afterForm);
